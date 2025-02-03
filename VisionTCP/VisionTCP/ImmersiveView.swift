229aa1c4efe69074c879170abcd5b50aeb0378df
Alessandra Souza <42846638+alelekime@users.noreply.github.com>
2025-02-03 10:12:31 +0100

    xcode project

---
+++ VisionTCP/VisionTCP/ImmersiveView.swift
@@ -0,0 +1,30 @@
+//
+//  ImmersiveView.swift
+//  VisionTCP
+//
+//  Created by Alessandra Souza da Silva on 03/02/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ImmersiveView: View {
+
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(immersiveContentEntity)
+
+                // Put skybox here.  See example in World project available at
+                // https://developer.apple.com/
+            }
+        }
+    }
+}
+
+#Preview(immersionStyle: .full) {
+    ImmersiveView()
+        .environment(AppModel())
+}
---
+++ VisionTCP/VisionTCP/VisionTCPApp.swift
@@ -0,0 +1,33 @@
+//
+//  VisionTCPApp.swift
+//  VisionTCP
+//
+//  Created by Alessandra Souza da Silva on 03/02/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionTCPApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
